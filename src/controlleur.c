619e77f15c9cab8c94b40441d764b2567f1ad0ab
IdirYah <mi_yahiaoui@esi.dz>
2025-02-25 17:00:35 +0100

    controller updated

--- src/controlleur.c
+++ src/controlleur.c
@@ -9,26 +9,36 @@ int comparerTuiles(tuileTypes p1,tuileTypes p2){
     return p2 == VILLE || p2 == BLASON;
 }
 //-------------------------
-int isPositionTuileValide(grille* g,tuile *t,int dx,int dy){
-    if(g->tabTuiles[dx][dy] != NULL){
+int isPositionTuileValide(grille* g, tuile *t, int dx, int dy) {
+    if (g->tabTuiles[dx][dy] != NULL) {
         return 0;
     }
-    if(g->tabTuiles[dx-1][dy] == NULL && g->tabTuiles[dx+1][dy] == NULL && g->tabTuiles[dx][dy-1] == NULL && g->tabTuiles[dx][dy+1] == NULL){
-        return 0;
+    int aVoisin = 0;
+    if (g->tabTuiles[dx-1][dy] != NULL) { 
+        aVoisin = 1;
+        if(!comparerTuiles(t->nord, g->tabTuiles[dx-1][dy]->sud)){
+            return 0;
+        }
     }
-    if(g->tabTuiles[dx-1][dy] != NULL && comparerTuiles(t->nord,g->tabTuiles[dx-1][dy]->sud) == 1){
-        return 1;
+    if(g->tabTuiles[dx+1][dy] != NULL) { 
+        aVoisin = 1;
+        if (!comparerTuiles(t->sud, g->tabTuiles[dx+1][dy]->nord)){
+            return 0;
+        }
     }
-    if(g->tabTuiles[dx+1][dy] != NULL && comparerTuiles(t->sud,g->tabTuiles[dx+1][dy]->nord) == 1){
-        return 1;
+    if(g->tabTuiles[dx][dy-1] != NULL) { 
+        aVoisin = 1;
+        if(!comparerTuiles(t->ouest, g->tabTuiles[dx][dy-1]->est)){
+            return 0;
+        }
     }
-    if(g->tabTuiles[dx][dy-1] != NULL && comparerTuiles(t->ouest,g->tabTuiles[dx][dy-1]->est) == 1){
-        return 1;
+    if(g->tabTuiles[dx][dy+1] != NULL) { 
+        aVoisin = 1;
+        if(!comparerTuiles(t->est, g->tabTuiles[dx][dy+1]->ouest)){
+            return 0;
+        }
     }
-    if(g->tabTuiles[dx][dy+1] != NULL && comparerTuiles(t->est,g->tabTuiles[dx][dy+1]->ouest) == 1){
-        return 1;
-    }
-    return 0;
+    return aVoisin;
 }
 //--------------------------
 posValid* empilerPosValid(posValid* tete,int l,int c){
@@ -64,3 +74,4 @@ posValid* positionsValid(grille *g,tuile* t){
     }
     return tete;
 }
+//---------------------------
