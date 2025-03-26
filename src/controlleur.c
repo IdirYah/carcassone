741b1721013963c30324c472c82d352ce34772d5
IdirYah <mi_yahiaoui@esi.dz>
2025-03-27 00:43:58 +0100

    main in idir

--- src/controlleur.c
+++ src/controlleur.c
@@ -1,5 +1,6 @@
 #include "../include/controlleur.h"
 #include "../include/tuile.h"
+#include "../include/meeple.h"
 #include <stdlib.h>
 //-------------------------
 int comparerTuiles(tuileTypes p1,tuileTypes p2){
@@ -75,3 +76,68 @@ posValid* positionsValid(grille *g,tuile* t){
     return tete;
 }
 //---------------------------
+int isVisited(int x,int y,posValid* tete){
+    posValid* p = tete;
+    while(p != NULL){
+        if(p->colonne == y && p->ligne == x){
+            return 1;
+        }
+        p = p->suivant;
+    }
+    return 0;
+}
+//---------------------------
+//Controller si on peut poser un meeple sur une route (parcours de route)
+int meepleRouteController(grille* g,int x,int y,posValid* tete){
+    if(isVisited(x,y,tete)){
+        return 1;
+    }
+    tuile *t = g->tabTuiles[x][y];
+    if (t == NULL){
+        return 1;  
+    }
+    if(t->meeples != NULL){
+        return 0;
+    }
+    tete = empilerPosValid(tete, x, y);
+    if (t->centre == ROUTE) {
+        if (t->nord == ROUTE && meepleRouteController(g, x - 1, y, tete) == 0) {
+            return 0;
+        }
+        if (t->sud == ROUTE && meepleRouteController(g, x + 1, y, tete) == 0) {
+            return 0;
+        }
+        if (t->est == ROUTE && meepleRouteController(g, x, y + 1, tete) == 0) {
+            return 0;
+        }
+        if (t->ouest == ROUTE && meepleRouteController(g, x, y - 1, tete) == 0) {
+            return 0;
+        }
+    }else{
+        if (t->nord == ROUTE) {
+            tuile *tNord = g->tabTuiles[x - 1][y];
+            if (tNord != NULL && tNord->sud == ROUTE && meepleRouteController(g, x - 1, y, tete) == 0) {
+                return 0;
+            }
+        }
+        if (t->sud == ROUTE) {
+            tuile *tSud = g->tabTuiles[x + 1][y];
+            if (tSud != NULL && tSud->nord == ROUTE && meepleRouteController(g, x + 1, y, tete) == 0) {
+                return 0;
+            }
+        }
+        if (t->est == ROUTE) {
+            tuile *tEst = g->tabTuiles[x][y + 1];
+            if (tEst != NULL && tEst->ouest == ROUTE && meepleRouteController(g, x, y + 1, tete) == 0) {
+                return 0;
+            }
+        }
+        if (t->ouest == ROUTE) {
+            tuile *tOuest = g->tabTuiles[x][y - 1];
+            if (tOuest != NULL && tOuest->est == ROUTE && meepleRouteController(g, x, y - 1, tete) == 0) {
+                return 0;
+            }
+        }
+    }
+    return 1;  
+}
